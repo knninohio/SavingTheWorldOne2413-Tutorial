ab8f22b00dd3cf87c65603f501d5ce4b7f1fc81e
knnsmelly <>
2023-03-07 04:29:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    data = None
+    numbers = [1, 2, 3, 4, 5]
+    with open('into.txt', 'r') as fiel:
+      msg = file.read()
+    if request.method == 'POST':
+      data = request.form['whatever']
+      with open('secret.txt','a') as file:
+        file.write(data + '\n')
+    return render_template('index.html', whatever=data, nums=numbers, intro=msg)
+
+
+app.run(host='0.0.0.0', port=81)
